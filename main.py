f520c8ea7511077ac00fee3f60364a5bba7e4995
Olivie <olivierrustige@gmail.com>
2024-04-16 19:32:51 +0200

    Webpagina bugs gefixt alles werkt nu

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import cv2
 app = Flask(__name__)
 
 model = YOLO("yolov8n.pt")
-line_points = [(20, 400), (1080, 400)]  # line or region points
 classes_to_count = [0, 2]  # person and car classes for count
 count = Value('i', 0)
 
@@ -16,7 +15,7 @@ def run_object_detection(ip, count):
     cap = cv2.VideoCapture(f"http://{ip}:81/stream")
     assert cap.isOpened(), "Error reading video file"
     w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
-
+    line_points = [(w * 3 / 4, h), (w * 3 / 4, 0)]
     # Video writer
     video_writer = cv2.VideoWriter("object_counting_output.avi",
                                    cv2.VideoWriter_fourcc(*'mp4v'),
@@ -41,7 +40,8 @@ def run_object_detection(ip, count):
 
         im0 = counter.start_counting(im0, tracks)
         video_writer.write(im0)
-        if counter.class_wise_count['perso']['in'] >= 1 or counter.class_wise_count['perso']['out'] >= 1:
+        if 'perso' in counter.class_wise_count:
+            if counter.class_wise_count['perso']['in'] >= 1 or counter.class_wise_count['perso']['out'] >= 1:
                 count.value += counter.class_wise_count['perso']['in']
                 count.value -= counter.class_wise_count['perso']['out']
                 counter.class_wise_count['perso']['in'] = 0
@@ -52,12 +52,11 @@ def run_object_detection(ip, count):
     cv2.destroyAllWindows()
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def run_object_detection_on_request():
     data = request.get_json()
     ip_address = data.get('ip')
     print(ip_address)
-
     # Run object detection in a separate process
     p = Process(target=run_object_detection, args=(ip_address, count))
     p.start()
