f24a731739bdbc12244ad94ca3a1127a2c31fe26
Olivie <olivierrustige@gmail.com>
2024-04-16 13:14:11 +0200

    Webserver multiprocessing value!

---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, request
+from multiprocessing import Process, Value
+from ultralytics import YOLO
+from ultralytics.solutions import object_counter
+import cv2
+
+app = Flask(__name__)
+
+model = YOLO("yolov8n.pt")
+line_points = [(20, 400), (1080, 400)]  # line or region points
+classes_to_count = [0, 2]  # person and car classes for count
+count = Value('i', 0)
+
+
+def run_object_detection(ip, count):
+    cap = cv2.VideoCapture(f"http://{ip}:81/stream")
+    assert cap.isOpened(), "Error reading video file"
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+    # Video writer
+    video_writer = cv2.VideoWriter("object_counting_output.avi",
+                                   cv2.VideoWriter_fourcc(*'mp4v'),
+                                   fps,
+                                   (w, h))
+
+    # Init Object Counter
+    counter = object_counter.ObjectCounter()
+    counter.set_args(view_img=True,
+                     reg_pts=line_points,
+                     classes_names=model.names,
+                     draw_tracks=True,
+                     line_thickness=2)
+
+    while cap.isOpened():
+        success, im0 = cap.read()
+        if not success:
+            print("Video frame is empty or video processing has been successfully completed.")
+            break
+        tracks = model.track(im0, persist=True, show=False,
+                             classes=classes_to_count)
+
+        im0 = counter.start_counting(im0, tracks)
+        video_writer.write(im0)
+        if counter.class_wise_count['perso']['in'] >= 1 or counter.class_wise_count['perso']['out'] >= 1:
+                count.value += counter.class_wise_count['perso']['in']
+                count.value -= counter.class_wise_count['perso']['out']
+                counter.class_wise_count['perso']['in'] = 0
+                counter.class_wise_count['perso']['out'] = 0
+
+    cap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+
+@app.route('/', methods=['POST'])
+def run_object_detection_on_request():
+    data = request.get_json()
+    ip_address = data.get('ip')
+    print(ip_address)
+
+    # Run object detection in a separate process
+    p = Process(target=run_object_detection, args=(ip_address, count))
+    p.start()
+    print("Object detection process started")
+    return 'Object detection process started.'
+
+@app.route('/current')
+def current():
+    print(count.value)
+    return str(count.value)
+
+@app.route('/update/<num>')
+def update(num):
+    count.value += int(num)
+    return "Done"
+
+if __name__ == '__main__':
+    app.run()
+
